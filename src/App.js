7a009f74a47eaf5a6dd6a1b35a925c6c92dded12
jeka0 <latosh02@mail.ru>
2023-03-11 18:35:24 +0300

    added login request

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
+import { Auth } from "./auth/authContext";
+import AuthorizedRoute from './security/AuthorizedRoute';
+import UnauthorizedRoute from './security/UnauthorizedRoute';
 import Login from "./pages/login/login.js";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={ <Login />}/>
-    </Routes>
+    <Auth>
+    <AuthorizedRoute>
+      <Routes>
+      <Route path="/" element={ <div>login</div>}/>
+      </Routes>
+    </AuthorizedRoute>
+    <UnauthorizedRoute>
+      <Routes>
+        <Route path="/" element={ <Login />}/>
+      </Routes>
+    </UnauthorizedRoute>
+  </Auth>
   );
 }
 
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,26 +1,41 @@
 import React, { useState } from "react";
+import { useAuth } from '../../auth/useAuth';
 import imgShow from "../../Images/eye.png";
 import imgHide from "../../Images/hide.png";
 import "./login.css";
 
 function Login(){
+    const [form, setForm] = useState({});
     const [showPassword, setShowPassword] = useState(false);
 
     const togglePasswordVisibility = ()=>{
         setShowPassword(!showPassword);
     }
 
+    const { login }= useAuth();
+
+    const onSubmit = (event)=>{
+        event.preventDefault();
+        login(form)
+        .catch((err)=>{
+            console.log("Wrong login or password");
+        });
+    }
+    const updateForm = async (event)=>{
+        setForm({...form, [event.target.name] : event.target.value})
+    }
+
     return(
         <div className="Background">
             <div className="content">
                 <p className="margin">Login</p>
-                <input className="input border"></input>
+                <input className="input border" name="login" onChange = {updateForm}></input>
                 <p className="margin">Password</p>
                 <div className="password_container border"> 
-                    <input className="input border_none" type={showPassword? "text":"password"}></input>
+                    <input className="input border_none" name="password" type={showPassword? "text":"password"} onChange = {updateForm}></input>
                     <input className="togglePassword" type="image" src={showPassword? imgHide:imgShow} alt="Кнопка «togglePassword»" onClick={togglePasswordVisibility}/>
                 </div>
-                <button className="margin">Login</button>
+                <button className="margin" onClick={onSubmit}>Login</button>
             </div>
         </div>
     );
